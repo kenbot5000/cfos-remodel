a5e82c23499c8df663360d89a42698e314cb20ea
Rionn Kenshin Pineda <rionnken@outlook.com>
2020-09-26 00:33:41 +0800

    Add student model and route

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-require('dotenv').config();
-const express = require('express');
-const consola = require('consola');
-const bodyParser = require('body-parser');
+require("dotenv").config();
+const express = require("express");
+const consola = require("consola");
+const bodyParser = require("body-parser");
 const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-const userRoute = require('./routes/userRoute')
-app.use('/user', userRoute);
+const userRoute = require("./routes/userRoute");
+app.use("/user", userRoute);
+
+const studentRoute = require("./routes/studentRoute");
+app.use("/student", studentRoute);
 
 const host = process.env.HOST;
 const port = process.env.PORT;
 app.listen(port, host);
 consola.ready({
   message: `Server listening on http://${host}:${port}`,
-  badge: true
+  badge: true,
 });
